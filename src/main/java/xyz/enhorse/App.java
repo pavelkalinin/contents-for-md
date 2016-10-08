d93a1925194ea1d0f1c7e19519b1934457675dc4
pavelkalinin <pavel13kalinin@gmail.com>
2016-10-08 12:48:32 +0500

    Create project structure

---
+++ src/main/java/xyz/enhorse/App.java
@@ -0,0 +1,12 @@
+package xyz.enhorse;
+
+/**
+ * @author <a href="mailto:pavel13kalinin@gmail.com">Pavel Kalinin</a>
+ *         08/10/16
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
